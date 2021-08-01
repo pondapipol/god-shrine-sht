cf6335f5acc999cffd0d49538891bf0e2d327277
pondapipol <pond1706@gmail.com>
2021-08-01 16:59:09 +0700

    add video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
+
+import Video from "../video/thai-dance.mp4";
 
 const Home = () => {
   return (
-    <div className="p-8 h-screen flex flex-col justify-center items-center bg-purple-400">
-      <div className="text-center text-white text-3xl font-semibold">
-        <div>Welcome to our digital shrine</div>
-        <div>We will make your dreams come true</div>
-      </div>
-      <div className="pt-6">
-        <Link to={'/choose-incent-quantity'}>
-          <button
-            className="rounded-full p-2 bg-purple-500 text-white font-semibold w-40 shadow"
-          >
-            GET STARTED
-          </button>
-        </Link>
+    <div className="w-screen h-screen" style={{ background: "#b68884" }}>
+      <video autoPlay muted loop className="fixed w-full h-full">
+        <source src={Video} type="video/mp4" />
+      </video>
+      <div className="p-8 w-screen h-screen flex flex-col justify-center items-center bg-black bg-opacity-50 absolute">
+        <div className="text-center text-white text-3xl font-semibold">
+          <div className="mb-1">Welcome to our digital shrine</div>
+          <div>We will make your dreams come true</div>
+        </div>
+        <div className="pt-8">
+          <Link to={"/choose-incent-quantity"}>
+            <button className="rounded-full p-2 bg-purple-500 text-white font-semibold w-40 shadow">
+              GET STARTED
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
